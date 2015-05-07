eb9e8207644599a413e3e42887fcdcecee437d2b
pat19880 <v19880@126.com>
2015-05-07 18:03:58 +0800

    php jpg压缩webp

--- PHP/Convert jpg to webp using imagewebp.php
+++ PHP/Convert jpg to webp using imagewebp.php
@@ -1,5 +1,6 @@
 <?php
 // jpg图片转webp
+// 需要php5.5以上支持
 $filename = dirname(__FILE__) .'/example.jpg';
 $im = imagecreatefromjpeg($filename);
 $webp =imagewebp($im, str_replace('jpg', 'webp', $filename));
